3d1654cb6328b783152b19ee06b574fda4ec52d9
MichelFernandes1997 <michelfsa@live.com>
2021-06-27 15:57:20 -0300

    dark mode 1.0.0

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#393E45',
+        secondary: '#3F88C5',
+
+        background: '#222',
+        text: '#fff'
+    }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#3F88C5',
+        secondary: '#393E45',
+
+        background: '#f5f5f5',
+        text: '#333'
+    }
+}
